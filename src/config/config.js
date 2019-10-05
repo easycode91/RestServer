b15d19afcfa2a04e7ef9af33e78b2f7745480a00
jhonatan <easycode91@gmail.com>
2019-10-05 10:39:55 -0500

    habilitando las bases de datos

--- src/config/config.js
+++ src/config/config.js
@@ -10,10 +10,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
 //CONNECTION
 //================================================= 
 let urldDb;
-//if(process.env.NODE_ENV === 'dev'){
-  //  urldDb = 'mongodb://localhost/cafe';
-//}else{
+if(process.env.NODE_ENV === 'dev'){
+  urldDb = 'mongodb://localhost/cafe';
+}else{
     urldDb = 'mongodb+srv://jhonatan:1077449704p@cluster0-j60p0.mongodb.net/cafe?retryWrites=true&w=majority'
-//}
+}
 
 process.env.URL_DB = urldDb;
